453d627deb518c874b482feac5aebe564942108e
Valenciaal <valeaal@uma.es>
2024-04-22 13:18:25 +0200

    Listar (sin webapp)

--- app_code/src/main/java/es/uma/proyectotaw/controller/AppController.java
+++ app_code/src/main/java/es/uma/proyectotaw/controller/AppController.java
@@ -1,18 +1,24 @@
 package es.uma.proyectotaw.controller;
 
-import ch.qos.logback.core.model.Model;
-import es.uma.proyectotaw.dao.ClienteRepository;
-import es.uma.proyectotaw.dao.ComidaRepository;
-import es.uma.proyectotaw.dao.EjercicioRepository;
+import org.springframework.ui.Model;
+import es.uma.proyectotaw.entity.*;
+import es.uma.proyectotaw.dao.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
 
 @Controller
 public class AppController {
 
+    @Autowired
+    protected EjercicioRepository ejercicioRepository;
+
     @GetMapping("/")
     public String inicio(Model model) {
+        List<Ejercicio> listaEjerciciosCompleta= this.ejercicioRepository.findAll();
+        model.addAttribute("lista", listaEjerciciosCompleta);
+        System.out.println(listaEjerciciosCompleta);
         return null;
     }
 
