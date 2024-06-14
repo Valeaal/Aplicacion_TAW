f6b9389908e62b6b740cd3096dea8c1a9dc88a9a
Miguel Galdeano <99599864+MikiGR@users.noreply.github.com>
2024-06-14 19:53:07 +0200

    Delete app_code directory

--- app_code/src/main/java/es/uma/proyectotaw/dao/Tipo_RutinaRepository.java
+++
@@ -1,7 +0,0 @@
-package es.uma.proyectotaw.dao;
-
-import es.uma.proyectotaw.entity.TipoRutina;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Tipo_RutinaRepository extends JpaRepository<TipoRutina, Integer> {
-}
